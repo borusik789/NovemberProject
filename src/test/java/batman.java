af6b14ba884d4b70954f392de6369dcac0ec67f3
borusik789 <borusik789@gmail.com>
2020-11-02 14:33:11 +0300

    car

--- src/test/java/batman.java
+++ src/test/java/batman.java
@@ -2,6 +2,9 @@ public class batman {
     public static void main(String[] args) {
         System.out.println("I will catch you Joker");
         //Yes we will
+        //Batman has cool
+
+
     }
 
 }
